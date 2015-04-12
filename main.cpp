ffe41834871becf9c65f650f94d2b57a9cc487a7
Nico John Alberti Williams <nijowill@unix2.lt.ucsc.edu>
2015-04-11 18:42:06 -0700

    Working on getting options parsing to work

---
+++ main.cpp
@@ -0,0 +1,127 @@
+//Nico Williams
+//nijowill
+//Assignment 1 - String Set and PreProcessor
+// Use cpp to scan a file and print line numbers.
+// Print out each input line read in, then strtok it for
+// tokens.
+
+#include <unistd.h>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <errno.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wait.h>
+#include <string>
+using namespace std;
+
+#include "stringset.h"
+#include "auxlib.h"
+
+
+const string CPP = "/usr/bin/cpp";
+const size_t LINESIZE = 1024;
+int exit_status = 0;
+extern int yy_flex_debug;
+extern int yydebug;
+extern FILE *yyin;
+extern int yylex(void);
+extern int yyparse(void);
+
+
+// Chomp the last character from a buffer if it is delim.
+void chomp (char* string, char delim) {
+   size_t len = strlen (string);
+   if (len == 0) return;
+   char* nlpos = string + len - 1;
+   if (*nlpos == delim) *nlpos = '\0';
+}
+
+// Run cpp against the lines of the file.
+void cpplines (FILE* pipe, char* filename) {
+   int linenr = 1;
+   char inputname[LINESIZE];
+   strcpy (inputname, filename);
+   for (;;) {
+      char buffer[LINESIZE];
+      char* fgets_rc = fgets (buffer, LINESIZE, pipe);
+      if (fgets_rc == NULL) break;
+      chomp (buffer, '\n');
+      printf ("%s:line %d: [%s]\n", filename, linenr, buffer);
+      // http://gcc.gnu.org/onlinedocs/cpp/Preprocessor-Output.html
+      int sscanf_rc = sscanf (buffer, "# %d \"%[^\"]\"",
+                              &linenr, filename);
+      if (sscanf_rc == 2) {
+         printf ("DIRECTIVE: line %d file \"%s\"\n", linenr, filename);
+         continue;
+      }
+      char* savepos = NULL;
+      char* bufptr = buffer;
+      for (int tokenct = 1;; ++tokenct) {
+         char* token = strtok_r (bufptr, " \t\n", &savepos);
+         bufptr = NULL;
+         if (token == NULL) break;
+         printf ("token %d.%d: [%s]\n",
+                 linenr, tokenct, token);
+      }
+      ++linenr;
+   }
+}
+
+int main (int argc, char** argv) {
+
+/*Get options. code based off
+gnu.org/software/libc/manual/html_node/Example-of-Getopt.html#Example-of-Getopt
+*/
+  int opts;
+  string debug;
+  FILE * inFile;
+  yy_flex_debug = 0;
+  while((opts = getopt(argc, argv, "ly:@:D")!=-1)){
+    switch(opts){
+    
+      case 'l':
+        yy_flex_debug = 1;
+        break;
+        
+      case 'y':
+        yydebug = 1;
+        break;
+        
+      case '@':
+        set_debugflags(optarg);
+        break;
+        
+      case 'D':
+        debug = optarg;
+        inFile = fopen("test.txt", "w");
+        
+
+
+
+
+
+
+
+/*    set_execname (argv[0]);
+   for (int argi = 1; argi < argc; ++argi) {
+      char* filename = argv[argi];
+      string command = CPP + " " + filename;
+      printf ("command=\"%s\"\n", command.c_str());
+      FILE* pipe = popen (command.c_str(), "r");
+      if (pipe == NULL) {
+         syserrprintf (command.c_str());
+      }else {
+         cpplines (pipe, filename);
+         int pclose_rc = pclose (pipe);
+         eprint_status (command.c_str(), pclose_rc);
+      }
+   } */
+   return get_exitstatus();
+}
+
+
+
---
+++ stringset.h
@@ -0,0 +1,15 @@
+//Nico Williams
+//nijowill
+//Assignment 1 - String Set and PreProcessor
+
+#ifndef __STRINGSET__
+#define __STRINGSET__
+
+#include <iostream>
+#include <string>
+
+const std::string* intern_stringset (const char*);
+
+void dump_stringset (std::ostream&);
+
+#endif
